68af6344e6dfa1a9f873a6da53fb9f3070bdd3b8
CharlieStanz7 <cstanczewski2005@gmail.com>
2025-09-25 11:28:10 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum = intOperandA + intOperandB;
+        int intProduct = intOperandA * intOperandB;
+        int intDifference = intOperandA - intOperandB;
+        int intQuotient = intOperandA / intOperandB;
+        int intModulo = intOperandA % intOperandB;
+        System.out.println("The sum of ints " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+
+
+
+        double doubleOperandA = 2.90;
+        double doubleOperandB = 3.50;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+        double doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The sum of doubles " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+
+
+        double myLunchCost = 12.50;
+        System.out.println("The lunch cost is " + myLunchCost);
+        int numFamKids = 3;
+        System.out.println("The number of fam Kids is " + numFamKids);
+        boolean isRaining = true;
+        if (isRaining) {System.out.println("It is raining");
+        double galGasPrice = 3.43;
+        System.out.println("The gas price is " + galGasPrice);
+        double shoeSize = 10.5;
+        System.out.println("The shoe size is " + shoeSize);
+        int birthMonth = 3;
+        System.out.println("The birth month is " + birthMonth);
+        String fullName = "Charlie Stanczewski";
+        System.out.println("My full name is " + fullName);
+
+
+        }
+
+    }
+}
